89b6c5f7357a1b58506b7bb111ddd78490303cd8
AutomationAlchemyst <aththaariq001@gmail.com>
2025-07-13 11:24:33 +0800

    Fix missing DELIVERY_FEE constant

--- src/app/event-builder/page.tsx
+++ src/app/event-builder/page.tsx
@@ -27,7 +27,7 @@ import { getBlockedDates } from '@/app/admin/manage-dates/actions';
 
 const DELIVERY_FEE = 45.00; // ADD THIS LINE BACK
 
-const EVENT_TIME_SLOTS = [ ... ];
+const EVENT_TIME_SLOTS = ['09:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '01:00 PM', '02:00 PM', '03:00 PM', '04:00 PM', '05:00 PM'];;
 
 // --- This interface is changed ---
 interface EventConfig {
