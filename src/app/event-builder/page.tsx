0bd8736558c3fd9c2c6487f186037b81998d3337
AutomationAlchemyst <aththaariq001@gmail.com>
2025-07-13 13:44:09 +0800

    full deployement.Eventconfig fixed

--- src/app/event-builder/page.tsx
+++ src/app/event-builder/page.tsx
@@ -867,7 +867,7 @@ export default function EventBuilderPage() {
         <PaymentConfirmationDialog
           isOpen={isPaymentModalOpen}
           onClose={resetBookingProcess}
-          eventConfig={currentEventConfig}
+          eventConfig={currentEventConfig as EventConfig & { eventDate: Date }}
           customerDetails={customerDetailsForPayment}
         />
       )}
